9be065906d1b68f835c318bcc88abb5c921a4856
RostreiD <RostreiD@computer-b4d4ca>
2017-03-06 09:02:53 +0200

    Add Config.class and dependencies in pom.xml

---
+++ Person-REST-Client/src/main/java/jp/hanazono/config/Config.java
@@ -0,0 +1,37 @@
+package jp.hanazono.config;
+
+import org.apache.taglibs.standard.tlv.JstlBaseTLV;
+import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "jp.hanazono")
+public class Config {
+
+	@Bean
+	public AutowiredAnnotationBeanPostProcessor postprocessor() {
+		return new AutowiredAnnotationBeanPostProcessor();
+	}
+	@Bean
+	public void addResourceHandler(ResourceHandlerRegistry registry) 
+	{
+		registry.addResourceHandler("/pages/**").addResourceLocations("/pages/");
+	}
+
+	@Bean
+	public InternalResourceViewResolver setupResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/pages/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+
+}
