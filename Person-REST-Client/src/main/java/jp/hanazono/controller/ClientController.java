34a9cb25b94aff9b0746f02eaa22dddae4c5dc98
RostreiD <RostreiD@computer-b4d4ca>
2017-03-06 12:43:36 +0200

    POST Method implemented.

--- Person-REST-Client/src/main/java/jp/hanazono/controller/ClientController.java
+++ Person-REST-Client/src/main/java/jp/hanazono/controller/ClientController.java
@@ -2,6 +2,8 @@ package jp.hanazono.controller;
 
 import java.util.Arrays;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,4 +23,44 @@ public class ClientController {
 		modelMap.addAttribute("Persons", Arrays.asList(persons));
 		return "index";
 	}
-}
+	
+	@RequestMapping("/edit")
+	public String editPerson(HttpServletRequest request,ModelMap modelMap)
+	{
+		if(request.getParameter("id")!=null)
+		{
+		Person person = new Person();
+		person.setId(Integer.parseInt(request.getParameter("id")));
+		person.setFirstname(request.getParameter("firstname"));
+		person.setLastname(request.getParameter("lastname"));
+		person.setAge(Integer.parseInt(request.getParameter("age")));
+		modelMap.addAttribute("person", person);
+		modelMap.addAttribute("method","PUT");
+		}
+		else
+		{
+			modelMap.addAttribute("method","POST");
+		}
+		return "edit";
+	}
+		
+		@RequestMapping("/save")
+		public String savePerson(HttpServletRequest request, ModelMap modelMap)
+		{
+			RestTemplate template = new RestTemplate();
+			Person person = new Person();
+			System.out.println(request.getParameter("firstname"));
+			if(request.getParameter("id")!="")
+			{
+			person.setId(Integer.parseInt(request.getParameter("id")));
+			}
+			person.setFirstname(request.getParameter("firstname"));
+			person.setLastname(request.getParameter("lastname"));
+			person.setAge(Integer.parseInt(request.getParameter("age")));
+			template.postForObject("http://localhost:8080/Person-REST-Service/", person, Person.class);
+			//Person persons[] = template.getForObject("http://localhost:8080/Person-REST-Service/",Person[].class);	
+			//modelMap.addAttribute("Persons", Arrays.asList(persons));
+			return "index"; 
+		}
+		
+	}
---
+++ Person-REST-Client/src/main/webapp/pages/edit.jsp
@@ -0,0 +1,38 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Add/Edit Person</title>
+</head>
+<body>
+	<h1>Add/Edit Person</h1>
+	<table border="5">
+		<TR>
+			<th>Person ID</th>
+			<th>Person Firstname</th>
+			<th>Person Lastname</th>
+			<th>Person Age</th>
+			<th>
+				<div>Action</div>
+			</th>
+		</TR>
+			<tr>
+			<form action="save?id=${person.id}&firstname=${person.firstname}&lastname=${person.lastname}&age=${person.age}" method="${method}">
+				<TD><input name="id" type="text" value="${person.id}" readonly="readonly"></TD>
+				<TD><input name="firstname" type="text" value="${person.firstname}"></TD>
+				<TD><input name="lastname" type="text" value="${person.lastname}"></TD>
+				<TD><input name="age" type="text" value="${person.age}"></TD>
+				<td>
+					<div>
+						<input type="image"  alt="save"
+							src="pages/images/save_icon_15.png" border="0">
+					</div>
+				</td>
+</form>
+			</tr>
+	</table>
+
+</body>
+</html>
